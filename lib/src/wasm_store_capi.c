8c7fb15319d86ce2fa08a67ff60f69331707ba9a
bfredl <bjorn.linse@gmail.com>
2025-03-14 13:00:17 +0100

    DYNAMIC

--- lib/src/wasm_store_capi.c
+++ lib/src/wasm_store_capi.c
@@ -42,6 +42,58 @@ TSWasmStore *ts_wasm_store_new(TSWasmEngine *engine, TSWasmError *wasm_error) {
   return store;
 }
 
+const uint8_t *
+wasm_runtime_get_custom_section(const wasm_module_t module,
+                                const char *name, uint32_t *len);
+
+typedef struct {
+  uint32_t memory_size;
+  uint32_t memory_align;
+  uint32_t table_size;
+  uint32_t table_align;
+} WasmDylinkInfo;
+
+static uint8_t read_u8(const uint8_t **p) {
+  return *(*p)++;
+}
+
+static inline uint64_t read_uleb128(const uint8_t **p, const uint8_t *end) {
+  uint64_t value = 0;
+  unsigned shift = 0;
+  do {
+    if (*p == end)  return UINT64_MAX;
+    value += (uint64_t)(**p & 0x7f) << shift;
+    shift += 7;
+  } while (*((*p)++) >= 128);
+  return value;
+}
+
+static bool parse_dylink(wasm_module_t *mod, WasmDylinkInfo *info) {
+  uint32_t section_size;
+  const uint8_t * dylink_section = wasm_runtime_get_custom_section(*mod, "dylink.0", &section_size);
+  if (!dylink_section) return false;
+  const uint8_t * section_end = dylink_section + section_size;
+  const uint8_t WASM_DYLINK_MEM_INFO = 0x1;
+
+  const uint8_t *p = dylink_section;
+  while (p < section_end) {
+    uint8_t subsection_type = read_u8(&p);
+    uint32_t subsection_size = read_uleb128(&p, section_end);
+    const uint8_t *subsection_end = p + subsection_size;
+    if (subsection_end > section_end) return false;
+    if (subsection_type == WASM_DYLINK_MEM_INFO) {
+      info->memory_size = read_uleb128(&p, subsection_end);
+      info->memory_align = read_uleb128(&p, subsection_end);
+      info->table_size = read_uleb128(&p, subsection_end);
+      info->table_align = read_uleb128(&p, subsection_end);
+      return true;
+    }
+    p = subsection_end;
+  }
+  return false;
+
+}
+
 const TSLanguage *ts_wasm_store_load_language(
   TSWasmStore *self,
   const char *language_name,
@@ -61,6 +113,16 @@ const TSLanguage *ts_wasm_store_load_language(
 
   wasm_extern_vec_t imports;
   wasm_extern_vec_new_uninitialized(&imports, import_types.size);
+
+  WasmDylinkInfo dylink_info;
+  if (!parse_dylink(mod->mod, &dylink_info)) {
+    fprintf(stderr, "fail:<\n");
+    abort();
+  }
+  fprintf(stderr, "DYNAMIC: %d, %d\n", dylink_info.memory_size, dylink_info.table_size);
+
+
+  wasm_table_t *functable;
 
   for (size_t i = 0; i < import_types.size; i++) {
     wasm_importtype_t *type = import_types.data[i];
