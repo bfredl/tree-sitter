464a864d78f31b15bba027dde2e9a9166edc1ba5
bfredl <bjorn.linse@gmail.com>
2025-03-17 09:14:39 +0100

    knowver

--- lib/src/wasm_store_capi.c
+++ lib/src/wasm_store_capi.c
@@ -29,6 +29,18 @@ typedef struct {
   uint32_t lexer_address;
 } LanguageWasmModule;
 
+// LexerInWasmMemory - The memory layout of a `TSLexer` when compiled to wasm32.
+// This is used to copy mutable lexing state in and out of the wasm memory.
+typedef struct {
+  int32_t lookahead;
+  TSSymbol result_symbol;
+  int32_t advance;
+  int32_t mark_end;
+  int32_t get_column;
+  int32_t is_at_included_range_start;
+  int32_t eof;
+} LexerInWasmMemory;
+
 struct TSWasmStore {
   wasm_engine_t *engine;
   // this is bullshit, just because the internal api abuses WASMStore as a
@@ -94,6 +106,10 @@ static bool parse_dylink(wasm_module_t *mod, WasmDylinkInfo *info) {
 
 }
 
+static bool name_eq(const wasm_name_t *name, const char *string) {
+  return strncmp(string, name->data, name->size) == 0;
+}
+
 const TSLanguage *ts_wasm_store_load_language(
   TSWasmStore *self,
   const char *language_name,
@@ -121,25 +137,52 @@ const TSLanguage *ts_wasm_store_load_language(
   }
   fprintf(stderr, "DYNAMIC: %d, %d\n", dylink_info.memory_size, dylink_info.table_size);
 
+  uint32_t lexer_in_mem = 2048; // this is arbitrary, but likely should not be zero
+  uint32_t memory_base = lexer_in_mem + sizeof(LexerInWasmMemory);
+  uint32_t stack_pointer = lexer_in_mem - 16;
 
   wasm_table_t *functable;
+
+  wasm_valtype_t *i32 = wasm_valtype_new_i32();
+  wasm_globaltype_t *glob_mut = wasm_globaltype_new(i32, true);
+  wasm_globaltype_t *glob_const = wasm_globaltype_new(i32, false);
+
+  int table_base = 5;
 
   for (size_t i = 0; i < import_types.size; i++) {
     wasm_importtype_t *type = import_types.data[i];
     const wasm_name_t *name = wasm_importtype_name(type);
+    wasm_extern_t *resolved = NULL;
     switch(wasm_externtype_kind(wasm_importtype_type(type))) {
       case WASM_EXTERN_FUNC:
         fprintf(stderr, "func: "); break;
-      case WASM_EXTERN_GLOBAL:
+      case WASM_EXTERN_GLOBAL:;
+        uint32_t value = 0;
+        bool mut = false;
+        if (name_eq(name, "__stack_pointer")) {
+          value = stack_pointer; mut = true;
+        } else if (name_eq(name, "__memory_base")) {
+          value = memory_base;
+        } else if (name_eq(name, "__table_base")) {
+          value = table_base;
+        } else {
+          goto importfail;
+        }
+        wasm_global_t *glob = wasm_global_new(mod->store, mut ? glob_mut : glob_const, &(wasm_val_t)WASM_I32_VAL(value));
+        resolved = wasm_global_as_extern(glob);
         fprintf(stderr, "global: "); break;
       case WASM_EXTERN_TABLE:
+        if (name_eq(name, "__indirect_function_table")) {
+        }
         fprintf(stderr, "table: "); break;
       case WASM_EXTERN_MEMORY:
         fprintf(stderr, "mem: "); break;
     }
+    imports.data[i] = resolved;
     fprintf(stderr, "%.*s\n", (int)name->size, name->data);
   }
 
+importfail:
   abort();
 }
 
